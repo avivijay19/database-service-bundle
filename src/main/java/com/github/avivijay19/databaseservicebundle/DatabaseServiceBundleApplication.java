4fbe968b6f94a4afb6e837c628f6ff858302637f
avinashvijayvargiya <avinash14022002@gmail.com>
2024-09-29 16:23:40 +0530

    initial commit

---
+++ src/main/java/com/github/avivijay19/databaseservicebundle/DatabaseServiceBundleApplication.java
@@ -0,0 +1,13 @@
+package com.github.avivijay19.databaseservicebundle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseServiceBundleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseServiceBundleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/avivijay19/databaseservicebundle/DatabaseServiceBundleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.avivijay19.databaseservicebundle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseServiceBundleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
